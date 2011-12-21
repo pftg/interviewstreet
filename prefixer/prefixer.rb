46a62f4a83a21939264c673215deecfd6f83d96d
Paul Nikitochkin <pftg@jetthoughts.com>
2011-12-21 23:46:07 +0200

    added specs and text expression file

--- prefixer/prefixer.rb
+++ prefixer/prefixer.rb
@@ -1,26 +1,9 @@
-IDENTIFIER_REGEXP = /[+-]?\d+|\w+/
+OPERANDS_REGEXP = /^[+-]?\d+|\w+$/
+CONSTANT_REGEXP = /^[+-]?\d+$/
 OPERATOR_REGEXP = /[+-\\*\\%\/^]/
+OPERANDS_COUNT = 2
 
-def operator_preced c
-    case c
-    when '^' then 5
-    when '!' then 4
-    when /[*\/%]/ then 3
-    when /[+-]/ then 2
-    else 0
-    end
-end
-
-def operator_left_assoc c
-    case c
-    when /[*\/%+-^]/ then true
-    else false
-    end
-end
-
-def operator_args_count token
-  token == '!' ? 1 : 2
-end
+OPERATOR_PRECEDENCES = Hash.new(0).merge({ '^' => 5, '*' => 3,  '/' => 3, '%' => 3, '+' => 2, '-' => 2 })
 
 def ident? token
   token.match /\d+|\w+/
@@ -33,75 +16,150 @@ def opertaor? token
   OPERATORS.include? token
 end
 
-def push_op_to_out op
-
+module Enumerable
+  def pop_to n, dest, &block
+    block ||= lambda {|a| a.reverse }
+    dest.push *block.call(self.pop(n))
+  end
 end
 
-def shunting_yard str
+def shunting_yard infix_expression
   output = []
   operation_stack = []
-  str.split.each do |token|
+
+  infix_expression.split.each do |token|
     case token
     when '(' then operation_stack << token
     when ')' then
-      right_brace = operation_stack.rindex('(')
-      output += operation_stack[right_brace + 1..-1].reverse
-      operation_stack = operation_stack[0 ... right_brace]
-    when IDENTIFIER_REGEXP then output << token
+      # Need to remove from stack and push to output all operators before '('
+      # And remove '(' from stack
+
+      # detect '(' in stack
+      last_open_brace_index = operation_stack.rindex('(')
+      raise "Not valid input expression: non expected ')'" unless last_open_brace_index
+
+      operators_count_after_open_brace = operation_stack.length - last_open_brace_index - 1
+
+      operation_stack.pop_to operators_count_after_open_brace, output
+
+      operation_stack.pop # Pop '('
+    when OPERANDS_REGEXP then output << token
     when OPERATOR_REGEXP then
       # Find less preceds operators then current operator
       ops = operation_stack.reverse.take_while do |op|
-        op_preced = operator_preced(op)
-        token_preced = operator_preced(token)
+        stack_operator_precedence = OPERATOR_PRECEDENCES[op]
+        current_token_precedence = OPERATOR_PRECEDENCES[token]
 
         is_operator = op =~ OPERATOR_REGEXP
-        is_operator && ( (operator_left_assoc(token) && (token_preced <= op_preced)) || (!operator_left_assoc(op) && (token_preced < op_preced)))
+
+        is_operator && (current_token_precedence <= stack_operator_precedence)
       end
 
       # Push less preced operators to the output
       unless ops.empty?
-        output += ops
-        operation_stack = operation_stack[0 ... -1 * ops.length]
+        operation_stack.pop_to ops.length, output
       end
 
       # add operator to the operator stack
       operation_stack << token
     end
-
   end
-  output += operation_stack.reverse
 
-  output
+  # Return output with all remain operators in stack
+  output + operation_stack.reverse
 end
 
 def eval_rpn_expression expression, &visitor
-  identifiers = []
+  # Setup default visitor
+  visitor ||= lambda {|op, operands| (operands << op).join(' ') }
+
+  operands_stack = []
   expression.each do |token|
     case token
-    when IDENTIFIER_REGEXP then
-      identifiers << token
+    when OPERANDS_REGEXP then
+      operands_stack << token
     when OPERATOR_REGEXP then
-      #take operators identifiers
-      arg_count = operator_args_count(token)
-      operands = identifiers[-1 * arg_count .. -1]
-      identifiers = identifiers[0...-1 * arg_count]
-      result = yield(token, operands)
-      identifiers << result
+      #take operators operands
+      raise "Bad RPN Expression: expected #{OPERANDS_COUNT} operands for operator #{token} in #{expression}" if operands_stack.size < OPERANDS_COUNT
+
+      operands = operands_stack.pop(OPERANDS_COUNT)
+      result = visitor.call(token, operands)
+      operands_stack << result
     end
   end
 
-  raise "Bad RPN Expression: there are > 1 operands without operators" if identifiers.size > 1
+  raise "Bad RPN Expression: there are > 1 operands_stack without operators in #{expression}" if operands_stack.size > 1
 
-  identifiers.first
+  operands_stack.first
 end
 
-p infix_expression = "3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3"
-rpn_expr = shunting_yard infix_expression
-p rpn_expr
-p eval_rpn_expression(rpn_expr) { |op, operands|
-  if operands.all? {|operand| operand =~ IDENTIFIER_REGEXP }
-    eval(operands.insert(1, SPECIAL_RUBY_OPERATORS_MAPS[op] || op).join(' ')).to_s
-  else
-    "( #{op} #{operands.join(' ')} )"
+if __FILE__ ==  $0
+  begin
+    infix_output = lambda {|op, operands| "( #{operands.insert(1, op).join(' ')} )" }
+    postfix_output = lambda {|op, operands| "#{operands.insert(2, op).join(' ')}" }
+    prefix_output = lambda {|op, operands| "( #{operands.insert(0, op).join(' ')} )" }
+
+    expression_simplifier_visitor_decorator = lambda{|output_format|
+      lambda { |op, operands|
+        #Check if operands simplest type: constants
+        if operands.all? {|operand| operand =~ CONSTANT_REGEXP }
+          eval(operands.insert(1, SPECIAL_RUBY_OPERATORS_MAPS[op] || op).join(' ')).to_s
+        else
+          if negative_constant = operands.index {|operand| operand =~ /^-/}
+            operands[negative_constant] = output_format.call('-', [0, operands[negative_constant].to_i.abs])
+          end
+
+          output_format.call(op, operands)
+        end
+      }
+    }
+
+    require 'optparse'
+
+    options = { :format => prefix_output }
+
+    optparse = OptionParser.new do|opts|
+      # Set a banner, displayed at the top
+      # of the help screen.
+      opts.banner = "Usage: ruby #{__FILE__} [options] file"
+
+      opts.on( '-p', '--postfix', 'Postfix form output' ) do
+        options[:format] = postfix_output
+      end
+
+      opts.on( '-i', '--infix', 'Infix form output' ) do
+        options[:format] = infix_output
+      end
+
+      opts.on( '-r', '--reduce', 'Reduce all expressions' ) do
+        options[:reduce] = true
+      end
+    end
+
+    optparse.parse!
+
+    expresion_visitor = options[:format]
+
+    if options[:reduce]
+      expresion_visitor = expression_simplifier_visitor_decorator.call(expresion_visitor)
+    end
+
+
+    filename = ARGV[0] || "expressions"
+    infix_expressions = File.new(filename)
+
+    infix_expressions.each_line do |infix_expression|
+      infix_expression.strip!
+      rpn_expression = shunting_yard infix_expression
+
+      result = eval_rpn_expression(rpn_expression) {|op, operands|
+        expresion_visitor.call(op, operands)
+      }
+      puts "#{infix_expression} becomes #{result}"
+    end
+
+
+    #rescue => e
+    #  puts "ERRORS: #{e.message}"
   end
-}
+end
--- prefixer/prefixer_spec.rb
+++ prefixer/prefixer_spec.rb
@@ -11,4 +11,34 @@ RSpec.configure do |config|
   config.mock_with :rr
 end
 
+describe "#shunting_yard" do
+  context "simple sum" do
+    it "first operand should be in first output cell" do
+      result = shunting_yard "3 + 2"
+      result.should == %w(3 2 +)
+    end
+  end
 
+  context "expression with braces" do
+    subject { shunting_yard "( 2 * ( 5 + 1 ) )"}
+
+    it "should evaluate brace expression first" do
+      mul_index = subject.index '*'
+      add_index = subject.index '+'
+      add_index.should < mul_index
+    end
+
+    it "should not add to output braces" do
+      subject.should_not include("(")
+    end
+  end
+end
+
+describe "#eval_rpn_expression" do
+  context "valid example" do
+    subject { eval_rpn_expression %w[3 2 +]}
+    it "should return '3 2 +'" do
+      subject.should == "3 2 +"
+    end
+  end
+end
